4c0c5be8225d75f289a86ee00d890d48c0d96369
Aditya Rahman <adityarahman032@gmail.com>
2021-07-07 21:17:14 +0800

    Collaboration Implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -70,7 +70,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       const note = await this._service.getNoteById(id);
 
       return {
@@ -104,12 +104,11 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
 
-      const { title, body, tags } = request.payload;
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
-      await this._service.editNoteById(id, { title, body, tags });
+      await this._service.verifyNoteAccess(id, credentialId);
+      await this._service.editNoteById(id, request.payload);
 
       return {
         status: 'success',
